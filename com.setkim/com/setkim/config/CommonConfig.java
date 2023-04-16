50748e7028b73efb6494d11d2289f1bd8f2422ef
Burak ÇELTİK <burak.celtik@ekinokssoftware.com>
2023-04-16 16:04:11 +0300

    Tablo soykırımı yapıldı

--- com.setkim/com/setkim/config/CommonConfig.java
+++ com.setkim/com/setkim/config/CommonConfig.java
@@ -1,7 +1,6 @@
 package com.setkim.config;
 
 public class CommonConfig {
-    //TODO: XML Parser for creating and saving config file
+    //TODO: XML Parser for creating and saving config file, tabi tabi yaşandı
     public static final String path = "/Users/keremapaydin/IdeaProjects/setkim/Deployment/setkim.db";
-    public static boolean isProduction = false;
 }
