13f8f23e651a53f2408f99625c8d9dac519f56ef
kerem <keremapaydin11@hotmail.com>
2023-04-16 16:30:33 +0300

    ekleme paneli değişti.

--- com.setkim/com/setkim/ekleme/EklemeController.java
+++ com.setkim/com/setkim/ekleme/EklemeController.java
@@ -24,28 +24,7 @@ public class EklemeController {
         siparisDetayController = new SiparisDetayController();
 
 
-        view.getBtnSec().addActionListener(e -> {
-            String selection = (String) view.getComboBox().getSelectedItem();
 
-            view.getWrapperPanel().removeAll();
-
-            if (selection.equals("Müşteri Bilgisi")){
-
-                view.getWrapperPanel().add(musteriController.getView());
-
-            } else if(selection.equals("Sipariş")) {
-
-                view.getWrapperPanel().add(siparisController.getView());
-
-            } else if (selection.equals("Sipariş Detayı")) {
-
-                view.getWrapperPanel().add(siparisDetayController.getView());
-
-            }
-
-            view.getWrapperPanel().revalidate();
-            view.getWrapperPanel().repaint();
-        });
     }
 
 
--- com.setkim/com/setkim/ekleme/EklemePanel.java
+++ com.setkim/com/setkim/ekleme/EklemePanel.java
@@ -1,51 +1,36 @@
 package com.setkim.ekleme;
 
-import java.awt.*;
-
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JComboBox;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class EklemePanel extends JPanel {
 
     private JPanel wrapperPanel;
     private JComboBox comboBox;
-    private JButton btnSec;
-
     public EklemePanel() {
         GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0 };
-        gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
-        gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 0.15, Double.MIN_VALUE };
-        gridBagLayout.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+        gridBagLayout.columnWidths = new int[]{0};
+        gridBagLayout.rowHeights = new int[]{0, 0};
+        gridBagLayout.columnWeights = new double[]{1.0};
+        gridBagLayout.rowWeights = new double[]{0.1, 1.0};
         setLayout(gridBagLayout);
 
         comboBox = new JComboBox();
-        comboBox.setModel(new DefaultComboBoxModel(new String[] { "Müşteri Bilgisi", "Sipariş", "Sipariş Detayı" }));
         GridBagConstraints gbc_comboBox = new GridBagConstraints();
-        gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+        gbc_comboBox.insets = new Insets(0, 0, 5, 0);
         gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
         gbc_comboBox.gridx = 0;
         gbc_comboBox.gridy = 0;
         add(comboBox, gbc_comboBox);
 
-        btnSec = new JButton("Seç");
-        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-        gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
-        gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
-        gbc_btnNewButton.gridx = 2;
-        gbc_btnNewButton.gridy = 0;
-        add(btnSec, gbc_btnNewButton);
-
         wrapperPanel = new JPanel();
         GridBagConstraints gbc_wrapperPanel = new GridBagConstraints();
-        gbc_wrapperPanel.gridwidth = 3;
-        gbc_wrapperPanel.insets = new Insets(0, 0, 0, 5);
         gbc_wrapperPanel.fill = GridBagConstraints.BOTH;
         gbc_wrapperPanel.gridx = 0;
         gbc_wrapperPanel.gridy = 1;
-
         add(wrapperPanel, gbc_wrapperPanel);
     }
 
@@ -53,16 +38,7 @@ public class EklemePanel extends JPanel {
         return wrapperPanel;
     }
 
-    public void setWrapperPanel(JPanel wrapperPanel) {
-        this.wrapperPanel = wrapperPanel;
-    }
-
     public JComboBox getComboBox() {
         return comboBox;
     }
-
-    public JButton getBtnSec() {
-        return btnSec;
-    }
-
 }
